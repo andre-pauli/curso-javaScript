6737e33b9c1882eb946c7796ddccf553e9e32293
André Pauli <andreps25@gmail.com>
2019-05-07 10:31:02 -0300

    Testando operadores lógicos no JS

--- js/principal.js
+++ js/principal.js
@@ -3,31 +3,15 @@ document.querySelector(".titulo").textContent = "Giovanna Nutricionista";
 var pacienteUm = document.querySelector("#primeiro");
 var peso = pacienteUm.querySelector(".info-peso").textContent;
 var altura = pacienteUm.querySelector(".info-altura").textContent;
-var imc = peso / (altura*altura);
-pacienteUm.querySelector(".info-imc").textContent = imc;
 
-var pacienteDois = document.querySelector("#segundo");
-var peso = pacienteDois.querySelector(".info-peso").textContent;
-var altura = pacienteDois.querySelector(".info-altura").textContent;
-var imc = peso / (altura*altura);
-pacienteDois.querySelector(".info-imc").textContent = imc;
+if(peso < 0 || peso > 400){
+    //console.log("Peso inválido!!");
+    pacienteUm.querySelector(".info-imc").textContent = "Peso inválido";
 
-var pacienteTres = document.querySelector("#terceiro");
-var peso = pacienteTres.querySelector(".info-peso").textContent;
-var altura = pacienteTres.querySelector(".info-altura").textContent;
-var imc = peso / (altura*altura);
-pacienteTres.querySelector(".info-imc").textContent = imc;
-
-var pacienteQuatro = document.querySelector("#quarto");
-var peso = pacienteQuatro.querySelector(".info-peso").textContent;
-var altura = pacienteQuatro.querySelector(".info-altura").textContent;
-var imc = peso / (altura*altura);
-pacienteQuatro.querySelector(".info-imc").textContent = imc;
-
-var pacienteCinco = document.querySelector("#quinto");
-var peso = pacienteCinco.querySelector(".info-peso").textContent;
-var altura = pacienteCinco.querySelector(".info-altura").textContent;
-var imc = peso / (altura*altura);
-pacienteCinco.querySelector(".info-imc").textContent = imc;
-
-
+} else if(altura < 0 || altura > 3){
+    pacienteUm.querySelector(".info-imc").textContent = "Altura inválida";
+} 
+else{
+    var imc = peso / (altura*altura);
+    pacienteUm.querySelector(".info-imc").textContent = imc;
+}
